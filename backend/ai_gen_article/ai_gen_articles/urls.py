d329e0a95b158e23925a2ee92c1c4472ddc13cf1
bishal <bishalbhaandari62@gmail.com>
2026-02-04 12:50:29 +0100

    Fix typo in URL configuration and add URL patterns for article_generator app

--- backend/ai_gen_article/ai_gen_articles/urls.py
+++ backend/ai_gen_article/ai_gen_articles/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('article_genetator.urls'))
+    path('', include('article_generator.urls'))
 ]
