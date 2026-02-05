65b127c6755440ae1fbe703326e52be621b048ad
bishal <bishalbhaandari62@gmail.com>
2026-02-05 12:59:43 +0100

    Update database configuration to use PostgreSQL instead of SQLite

--- backend/ai_gen_article/ai_gen_articles/settings.py
+++ backend/ai_gen_article/ai_gen_articles/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'ai_gen_articles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'article_database',
+        'USER': 'postgres',
+        'PASSWORD': 'SpfUp3iE8IQA99yc9D7Sw-XSb1IHRQrx',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
